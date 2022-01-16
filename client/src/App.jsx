3ee7131b1ac315612d21d4627c9eada7a5789241
Tara Saleh <taraneh.saleh@gmail.com>
2022-01-15 21:08:57 -0500

    tailwind and front setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const App = () => {
+    return <div className="container mx-auto border">
+        <h1>This is gonna be awsome! You Watch</h1>
+        <h1 className="text-red-500 font-bold underline">
+                Hello world!
+         </h1>
+        <div className="table"></div>
+        </div>
+}
+ 
+export default App;
